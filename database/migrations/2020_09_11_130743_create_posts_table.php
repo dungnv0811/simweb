329e59a36958f282bbc32d5788e731645e0525f2
Dung Van Nguyen <Dung.Van.Nguyen@mgm-tp.com>
2020-09-11 20:23:18 +0700

    create database

---
+++ database/migrations/2020_09_11_130743_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('slug')->unique();
+            $table->string('title', 100);
+            $table->string('image');
+            $table->string('short_description', 160);
+            $table->longText('description');
+            $table->boolean('published')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
