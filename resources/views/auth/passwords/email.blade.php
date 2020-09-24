44ce58a0f76e608e338ac076fe44f89b7f7d8619
Dung Van Nguyen <Dung.Van.Nguyen@mgm-tp.com>
2020-09-24 23:08:55 +0700

    fix padding css

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="container" style="padding-top: 60px;">
+<div class="container" style="padding-top: 60px; padding-bottom: 60px;">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
