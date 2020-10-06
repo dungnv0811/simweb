a2986c8165a9175adbc0021c8f59766f9c8d56b4
Tung Phan <pttungbk@gmail.com>
2020-10-06 07:15:56 +0700

    update code

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -68,13 +68,6 @@
                                 <a class="btn btn-info" href="{{ route('social.login', ['provider' => 'facebook']) }}">
                                 <i class="fa fa-facebook"></i>&nbsp; Đăng nhập bằng Facebook</a>
                             </div>
-                            @if ($errors->any())
-                            <div class="alert alert-danger text-center">
-                                @foreach ($errors->all() as $error)
-                                <div>{{ $error }}</div>
-                                @endforeach
-                            </div>
-                            @endif
                         </div>
                     </form>
                 </div>
